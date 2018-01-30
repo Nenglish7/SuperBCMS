40fc428cc271d1a2bc28a9161f2a6386708c9135
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-30 16:53:51 -0500

    Update server.php

--- server.php
+++ server.php
@@ -1,1 +1,20 @@
+<?php
+/**
+ * This file is a part of SuperBCMS.
+ *
+ * @author Nicholas English (Owner/Developer) <nenglish0820@outlook.com>.
+ *
+ * @link    <https://github.com/Nenglish7/SuperBCMS> Github repository.
+ * @license <https://github.com/Nenglish7/SuperBCMS/blob/master/LICENSE> MIT license.
+ */
 
+define('SYSTEM_ROOT', __DIR__);
+
+if (file_exists(SYSTEM_ROOT . '/vendor/autoload.php'))
+{
+    throw new RuntimeException(
+        'You need to run composer install or else the system will not run.'
+    );
+}
+
+require SYSTEM_ROOT . '/vendor/autoload.php';
