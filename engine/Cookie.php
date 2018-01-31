7eaf1e159c67d1c4a4e2789e9cc9884028b426bb
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-30 20:08:26 -0500

    Update Cookie.php

--- engine/Cookie.php
+++ engine/Cookie.php
@@ -1,1 +1,90 @@
+<?php
+declare(strict_types=1);
+/**
+ * This file is a part of SuperBCMS.
+ *
+ * @author Nicholas English (Owner/Developer) <nenglish0820@outlook.com>.
+ *
+ * @link    <https://github.com/Nenglish7/SuperBCMS> Github repository.
+ * @license <https://github.com/Nenglish7/SuperBCMS/blob/master/LICENSE> MIT license.
+ */
+ 
+namespace Nenglish7\Engine;
 
+use ParagonIE\Halite\Cookie as CookieController;
+use ParagonIE\Halite\Symmetric\EncryptionKey;
+use ParagonIE\Halite\HiddenString;
+
+/**
+ * Cookie.
+ */
+class Cookie implements CookieInterface
+{
+    
+    /**
+     * @var object $cookieController The cookie controller.
+     */
+    private $cookieController;
+    
+    /**
+     * Set the cookie encryption key.
+     *
+     * @codeCoverageIgnore
+     *
+     * @return void.
+     */
+    function __construct()
+    {
+        $this->cookieController = new CookieController(new EncryptionKey(new HiddenString(\CONFIG['application']['secret'])));
+    }
+    
+    /**
+     * Store a new cookie.
+     *
+     * @param string $name   The name of the cookie.
+     * @param mixed  $value  The cookie value.
+     * @param int    $expire The cookie expiration time.
+     * 
+     * @codeCoverageIgnore
+     *
+     * @return void.
+     */
+    public function store($name, $value, $expire): void
+    {
+        $this->cookieController->store($name, $value, $expire, \CONFIG['cookie']['path'], \CONFIG['cookie']['domain'], \CONFIG['cookie']['secure']);
+    }
+    
+    /**
+     * Fetch a cookie by name.
+     *
+     * @param string $name The name of the cookie.
+     *
+     * @codeCoverageIgnore
+     *
+     * @return mixed The cookie value.
+     */
+    public function fetch($name)
+    {
+        $this->cookieController->fetch($name);
+    }
+    
+    /**
+     * Delete a cookie by name.
+     *
+     * @param string $name The name of the cookie.
+     *
+     * @codeCoverageIgnore
+     *
+     * @return void.
+     */
+    public function fetch($name): void
+    {
+        if (isset($_COOKIE[$name]))
+        {
+            unset($_COOKIE[$name]);
+            
+            \setcookie($name, '', \time() - 42000, \CONFIG['cookie']['path'], \CONFIG['cookie']['domain'], \CONFIG['cookie']['secure'], \true);
+        }
+    }
+    
+}
