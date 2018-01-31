21e99afb09c8da76283d878a00054b98c08a2aad
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-30 19:20:00 -0500

    Create InvalidArgumentException.php

---
+++ engine/Exception/InvalidArgumentException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is a part of SuperBCMS.
+ *
+ * @author Nicholas English (Owner/Developer) <nenglish0820@outlook.com>.
+ *
+ * @link    <https://github.com/Nenglish7/SuperBCMS> Github repository.
+ * @license <https://github.com/Nenglish7/SuperBCMS/blob/master/LICENSE> MIT license.
+ */
+ 
+namespace Nenglish7\Engine\Exception;
+
+/**
+ * InvalidArgumentException.
+ */
+class InvalidArgumentException extends \InvalidArgumentException implements ExceptionInterface
+{
+}
