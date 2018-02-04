b3819deef5a086312f3b44bef1fc97846e770bef
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-02-03 23:00:22 -0500

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,11 @@
 <?php
+/**
+ * This file is a part of SuperBCMS.
+ *
+ * @author Nicholas English (Owner/Developer) <nenglish0820@outlook.com>.
+ *
+ * @link    <https://github.com/Nenglish7/SuperBCMS> Github repository.
+ * @license <https://github.com/Nenglish7/SuperBCMS/blob/master/LICENSE> MIT license.
+ */
 
 require 'vendor/autoload.php';
